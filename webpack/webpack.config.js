1b9699d27386b32b5254f702be42cabea5c78121
Jack <jackbennett0708@gmail.com>
2022-09-16 18:32:50 +0100

    Initial commit

---
+++ webpack/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    entry: {
+        background: path.resolve(__dirname, "..", "src", "background.ts"),
+    },
+    output: {
+        path: path.join(__dirname, "../dist"),
+        filename: "[name].js",
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        // copy files from the /public/ folder into the output folder
+        new CopyPlugin({
+            patterns: [{from: ".", to: ".", context: "public"}]
+        }),
+    ],
+};
